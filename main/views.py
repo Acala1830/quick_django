3fdd41160313169d04eb6c56d668129c40a69bef
Acala1830 <test00314@gmail.com>
2025-01-06 16:07:14 +0900

    初回コミット

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# a.リクエスト情報を受け取る
+def index(request):
+    # b.レスポンスを生成する
+    return HttpResponse('こんにちは、世界！')
+# 続きは2.2～
