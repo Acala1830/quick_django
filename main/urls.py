b8c84a0b34ff8aeaa29976ff5244a5d63bf118db
Willosa <willosa1830@gmail.com>
2025-01-07 21:30:03 +0900

    Part2完了

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,6 @@ from . import views
 # ルーティング情報を列挙する
 urlpatterns = [
     path('', views.index, name='index'),
+    path('temp', views.temp, name='temp'),
+    path('list', views.list, name='list'),
 ]
