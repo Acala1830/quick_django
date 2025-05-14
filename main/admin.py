143f42d71cdc40efd3cb125cf92a6c67000ce3f6
Willosa <willosa1830@gmail.com>
2025-05-14 15:33:05 +0900

    Part4完了

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 from .models import Book
+from .models import Review
+from .models import Author
 
 admin.site.register(Book)
+
+admin.site.register(Review)
+
+admin.site.register(Author)
